95ea64d96b2a5c68b3cb8efbe38563249b04efa7
CrisValhas <cristianrubiles@gmail.com>
2022-11-22 17:05:03 -0300

    fix apikey

--- src/pages/Blogger.jsx
+++ src/pages/Blogger.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Header } from '../components';
+// import {GOOGLE_API_KEY} from 'dotenv';
+
 
 export default function Blogger() {
 
@@ -9,10 +11,8 @@ export default function Blogger() {
     
     useEffect(() => {
         const reqData = async () => {
-            // const blogData = await axios.get("https://www.googleapis.com/blogger/v3/blogs/5453241076183956423?key=AIzaSyAhmbbj55QBFn2jVcrFeRd9XOEmjV11xpg")
-            const postData = await axios.get("https://www.googleapis.com/blogger/v3/blogs/5453241076183956423/posts?key=AIzaSyAhmbbj55QBFn2jVcrFeRd9XOEmjV11xpg")
+            const postData = await axios.get('https://www.googleapis.com/blogger/v3/blogs/5453241076183956423/posts?key='+ procces.env.GOOGLE_API_KEY)
             setData(postData.data.items) 
-            // console.log(data)
         };
         reqData()
     }, []);
@@ -26,7 +26,7 @@ export default function Blogger() {
             {data && 
             
             data.map((item, index) => {
-                console.log(item)
+                // console.log(item)
 
                 return (
                     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-2xl" id={index}>
